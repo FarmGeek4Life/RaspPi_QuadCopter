181d39c81e0504339237bf4b0a294110217b54f6
Bryson Gibbons <brysoncg@gmail.com>
2014-04-10 12:06:02 -0600

    Updated to add robustness.

--- gstreamer_build.sh
+++ gstreamer_build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#http://wiki.oz9aec.net/index.php/Building_Gstreamer_1.2_from_source
+
+NO_ERROR=0
+BASE_PATH=/home/pi/gst
+
+if [ ! -f dependency_install ]; then
+   apt-get install libx264-dev libgudev-1.0-dev yasm bison flex
+   touch dependency_install
+fi
 
 source ./set_env.sh
 FILES=( http://gstreamer.freedesktop.org/src/orc/orc-0.4.18.tar.gz \
@@ -9,55 +18,134 @@ FILES=( http://gstreamer.freedesktop.org/src/orc/orc-0.4.18.tar.gz \
         http://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-1.2.3.tar.xz \
         http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.2.3.tar.xz )
 
-for file in ${FILES[@]}; do
-   wget -c $file
-   wget -c $file.sha256sum
-done
-
-sha256sum -c *.sha256sum
+if [ ! -f downloaded ]; then
+   for file in ${FILES[@]}; do
+      [ $NO_ERROR -eq 0 ] && wget -c $file || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && wget -c $file.sha256sum || NO_ERROR=1
+   done
+   [ $NO_ERROR -eq 0 ] && sha256sum -c *.sha256sum || NO_ERROR=1
+   [ $NO_ERROR -eq 0 ] && touch downloaded
+fi
 
 for file in $(ls -1 | grep -E -e "gz$|xz$"); do
    #echo "$(echo "$file" | sed 's/\.tar\.[gx]z$//')"
    if [ ! -d $(echo "$file" | sed 's/\.tar\.[gx]z$//') ]; then
       echo "Extracting $file"
-      tar xf $file
+      [ $NO_ERROR -eq 0 ] && tar xf $file || NO_ERROR=1
    fi
 done
 
 PREFIX="--prefix=/home/pi/gst/runtime"
 ORC="--enable-orc"
 
-pushd orc*
-   ./configure $PREFIX
-   make install
+pushd $BASE_PATH/orc*
+   CURRENT=$BASE_PATH/orc_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gstreamer*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gstreamer*
+   CURRENT=$BASE_PATH/gstreamer_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gst-plugins-base*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gst-plugins-base*
+   CURRENT=$BASE_PATH/gst-plugins-base_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gst-plugins-good*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gst-plugins-good*
+   CURRENT=$BASE_PATH/gst-plugins-good_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gst-plugins-bad*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gst-plugins-bad*
+   CURRENT=$BASE_PATH/gst-plugins-bad_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gst-plugins-ugly*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gst-plugins-ugly*
+   CURRENT=$BASE_PATH/gst-plugins-ugly_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
 
-pushd gst-libav*
-   ./configure $ORC $PREFIX
-   make install
+pushd $BASE_PATH/gst-libav*
+   CURRENT=$BASE_PATH/gst-libav_status
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "configured") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && ./configure $ORC $PREFIX || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "configured" > $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "built") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "built" >> $CURRENT
+   fi
+   if [ ! -f $CURRENT ] || [ $(cat $CURRENT | grep -c "installed") -eq 0 ]; then
+      [ $NO_ERROR -eq 0 ] && make install || NO_ERROR=1
+      [ $NO_ERROR -eq 0 ] && echo "installed" >> $CURRENT
+   fi
 popd
